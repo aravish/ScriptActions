908fc334605a1373e91108d34cb38eede1730220
aravish <43964587+aravish@users.noreply.github.com>
2018-11-27 15:22:16 -0600

    Update installpackages.sh

--- installpackages.sh
+++ installpackages.sh
@@ -4,3 +4,4 @@ sudo /usr/bin/anaconda/bin/python -m pip install pandas==0.23.4
 sudo /usr/bin/anaconda/bin/python -m pip install azure-mgmt-resource
 sudo /usr/bin/anaconda/bin/python -m pip install azure-mgmt-datalake-store
 sudo /usr/bin/anaconda/bin/python -m pip install azure-datalake-store
+sudo /usr/bin/anaconda/bin/python -m pip install pyarrow
