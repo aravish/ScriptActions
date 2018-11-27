bcc05f85825b30874c6cd737fb8b00edfbd9060f
aravish <43964587+aravish@users.noreply.github.com>
2018-11-27 14:46:06 -0600

    Add files via upload

---
+++ installpackages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo /usr/bin/anaconda/bin/python -m pip install pandas==0.23.4
+sudo /usr/bin/anaconda/bin/python -m pip install azure-mgmt-resource
+sudo /usr/bin/anaconda/bin/python -m pip install azure-mgmt-datalake-store
+sudo /usr/bin/anaconda/bin/python -m pip install azure-datalake-store
