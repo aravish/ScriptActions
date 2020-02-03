27f8e1f3a5bde2e0d1d45a5a26623af8285784c5
Arvind Ravish <43964587+aravish@users.noreply.github.com>
2020-02-03 17:31:34 -0600

    Update condascriptaction.sh

--- condascriptaction.sh
+++ condascriptaction.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-/usr/bin/anaconda/envs/py35/bin/conda install -c conda-forge elephas keras matplotlib numpy pandas plotly scipy sklearn snowflake tensorflow -n py35
+/usr/bin/anaconda/envs/py35/bin/conda install -c conda-forge elephas keras matplotlib numpy pandas plotly scipy tensorflow -n py35
